c259c94caa368dd070f841956a36632064206579
nedac1 <nc824887@gmail.com>
2024-01-07 19:35:08 +0100

    dodate 3 migracije za tabele exercises i workouts

---
+++ laravelApp/database/migrations/2024_01_07_183237_create_exercises_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exercises', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('video_url')->nullable();
+            $table->integer('average_calories_burned')->nullable();
+            $table->string('category')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exercises');
+    }
+};
