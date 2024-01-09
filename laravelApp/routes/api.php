92c7ee998fea6aba24be684357e40631381f2860
nedac1 <nc824887@gmail.com>
2024-01-09 21:21:46 +0100

    autentifikacija

--- laravelApp/routes/api.php
+++ laravelApp/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ExerciseController;
 use App\Http\Controllers\HydratationController;
 use Illuminate\Http\Request;
@@ -15,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
  
 Route::prefix('exercises')->group(function () {
