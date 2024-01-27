bf157ed4e9c539025fe5f9ee4de38bf8f7d723e4
nedac1 <nc824887@gmail.com>
2024-01-27 16:02:55 +0100

    dodate rute

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,15 +1,18 @@
  
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './komponente/LoginRegistracija/LoginForm';
 import RegisterForm from './komponente/LoginRegistracija/RegisterForm';
  
 
 function App() {
   return (
-    <div className="App">
-       <LoginForm></LoginForm>
-       <RegisterForm></RegisterForm>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+      </Routes>
+    </Router>
   );
 }
 
