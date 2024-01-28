c25ef09fd36ecefeeb1f8c08d9d8c92e092f038d
Milica735 <milica.stojanovic735@gmail.com>
2024-01-28 19:48:44 +0100

    dodata mogucnost pretrage

--- reactapp/src/komponente/vezbe/Vezbe.jsx
+++ reactapp/src/komponente/vezbe/Vezbe.jsx
@@ -5,42 +5,52 @@ import useVezbe from '../hooks/useVezbe';
 import ExerciseRow from './ExerciseRow';
 
 const Vezbe = () => {
- 
   const [currentPage, setCurrentPage] = useState(1);
   const [exercisesPerPage] = useState(5);
+  const [searchTerm, setSearchTerm] = useState('');
   const token = sessionStorage.getItem('token');
-
-//   const [vezbe, setVezbe] = useState([]);
-//   useEffect(() => {
-//     axios.get('http://127.0.0.1:8000/api/exercises', {
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//       },
-//     })
-//       .then((response) => {
-//         setVezbe(response.data.data);
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//       });
-//   }, [token]);
-
   const vezbe = useVezbe('http://127.0.0.1:8000/api/exercises', token);
-  const indexOfLastExercise = currentPage * exercisesPerPage;
-  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
-  const currentExercises = vezbe.slice(indexOfFirstExercise, indexOfLastExercise);
+
  
+
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    setCurrentPage(1); // Resetujemo trenutnu stranu nakon pretrage
+  };
+
+  const filteredExercises = vezbe.filter((vezba) => {
+    const { name, description, video_url, average_calories_burned, category } = vezba;
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+
+    return (
+      name.toLowerCase().includes(lowerCaseSearchTerm) ||
+      description.toLowerCase().includes(lowerCaseSearchTerm) ||
+      video_url.toLowerCase().includes(lowerCaseSearchTerm) ||
+      average_calories_burned.toString().includes(lowerCaseSearchTerm) ||
+      category.toLowerCase().includes(lowerCaseSearchTerm)
+    );
+  });
+
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(vezbe.length / exercisesPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(filteredExercises.length / exercisesPerPage); i++) {
     pageNumbers.push(i);
   }
-
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = filteredExercises.slice(indexOfFirstExercise, indexOfLastExercise);
   return (
     <div className="vezbe-container">
       <h1>Vezbe</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Pretraži vezbe..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
       <table className="vezbe-table">
         <thead>
           <tr>
@@ -53,16 +63,16 @@ const Vezbe = () => {
           </tr>
         </thead>
         <tbody>
-         {currentExercises.map((vezba) => (
-            <ExerciseRow key={vezba.id} vezba={vezba} />  
+          {currentExercises.map((vezba) => (
+            <ExerciseRow key={vezba.id} vezba={vezba} />
           ))}
         </tbody>
       </table>
       <nav>
-        <ul className='pagination'>
-          {pageNumbers.map(number => (
-            <li key={number} className='page-item'>
-              <button onClick={() => paginate(number)}   className='page-link'>
+        <ul className="pagination">
+          {pageNumbers.map((number) => (
+            <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
+              <button onClick={() => paginate(number)} className="page-link">
                 {number}
               </button>
             </li>
